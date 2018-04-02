c72ce8c3bded4c245012a1bb1c832896ce22a7a1
Domenico Verde <domenico.verde@aol.com>
2018-04-02 21:13:40 +0200

    + [Unstable] Add Ese16

---
+++ Ese16/ListaPersone.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListaPersone {
+   public static void main(String[] args) {
+      ArrayList<Persona> people = new ArrayList<Persona>();
+      Scanner in = new Scanner(System.in);
+      String nome = "", cognome = "";
+      int eta = -1, tmp = 0;
+      char sesso;
+
+      for ( ; ; ) {
+         System.out.print("Insert name: [insert -1 HERE to stop] ");
+         nome = in.nextLine();
+         if (nome.equals("-1")) {
+            break;
+         }
+         System.out.print("Insert surname: ");
+         cognome = in.nextLine();
+         System.out.print("Insert age: ");
+         do {
+            try {
+               eta = in.nextInt();
+            } catch (InputMismatchException e) {
+               System.out.println("Error: wrong values!");
+            }
+         } while (eta <= 0);
+         in.nextLine();
+         System.out.print("Insert sex ");
+         do {
+            sesso = in.nextLine().charAt(0);
+         } while (sesso != 'f' || sesso != 'm' || sesso != 'u');
+
+         System.out.println("Adding...\n");
+         people.add(new Persona(nome, cognome, eta, sesso));
+      }
+
+      System.out.println("How do you want to sort these people? ");
+      System.out.print("1: Per Nome\n2: Per Cognome\n3: Per Nome Completo\n4: Per Eta'\n5: Per Genere\n -> ");
+      do {
+         tmp = in.nextInt();
+         in.nextLine();
+      } while (tmp < 1 || tmp > 5);
+
+      for (int i = 0; i < people.size(); i++) {
+         switch (tmp) {
+            case 1:
+               people.get(i).setOrdinamento(Ordinamento.perNome);
+               break;
+            case 2:
+               people.get(i).setOrdinamento(Ordinamento.perCognome);
+               break;
+            case 3:
+               people.get(i).setOrdinamento(Ordinamento.perNomeCompleto);
+               break;
+            case 4:
+               people.get(i).setOrdinamento(Ordinamento.perEta);
+               break;
+            case 5:
+               people.get(i).setOrdinamento(Ordinamento.perGenere);
+               break;
+         }
+      }
+
+      for (int i = 0; i < people.size()-1; i++) {
+         people.sort(people.get(i).compareTo(people.get(i+1)));
+      }
+   }
+}
---
+++ Ese16/Persona.java
@@ -0,0 +1,119 @@
+enum Ordinamento {
+   perNome,
+   perCognome,
+   perNomeCompleto,
+   perEta,
+   perGenere
+}
+
+public class Persona implements Comparable<Persona> {
+   protected String nome, cognome;
+   protected int eta;
+   protected char sesso;
+   protected Ordinamento ordinaPer;
+
+   Persona(String name, String surname, int age, char sex) {
+      nome = name;
+      cognome = surname;
+      eta = age;
+      sesso = sex;
+      ordinaPer = Ordinamento.perNomeCompleto;
+   }
+
+   public String getNome() {
+      return nome;
+   }
+
+   public void setNome(String name) {
+      nome = name;
+   }
+
+   public String getCognome() {
+      return cognome;
+   }
+
+   public void setCognome(String surname) {
+      cognome = surname;
+   }
+
+   public int getEta() {
+      return eta;
+   }
+
+   public void setEta(int age) {
+      eta = age;
+   }
+
+   public char getSesso() {
+      return nome;
+   }
+
+   public void setSesso(char sex) {
+      sesso = sex;
+   }
+
+   public Ordinamento getOrdinamento() {
+      return ordinaPer;
+   }
+
+   public void setOrdinamento(Ordinamento newOrder) {
+      ordinaPer = newOrder;
+/*
+      switch (newOrder) {
+         case perNome:
+            ordinaPer = Ordinamento.perNome;
+         case perCognome:
+            ordinaPer = Ordinamento.perCognome;
+         case perNomeCompleto:
+            ordinaPer = Ordinamento.perNomeCompleto;
+         case perEta:
+            ordinaPer = Ordinamento.perEta;
+         case perGenere:
+            ordinaPer = Ordinamento.perGenere;
+         default:
+            ordinaPer = Ordinamento.perNomeCompleto;
+      }
+*/
+   }
+
+   public int compareTo(Persona p) {
+      switch (ordinaPer) {
+         case perNome:
+            if (this.nome.compareTo(p.getNome()) == 0) {
+               return 0;
+            } else {
+               return this.nome.compareTo(p.getNome());
+            }
+         case perCognome:
+            if (this.cognome.compareTo(p.getCognome()) == 0) {
+               return 0;
+            } else {
+               return this.cognome.compareTo(p.getCognome());
+            }
+         case perNomeCompleto:
+            if (this.nome.compareTo(p.getNome()) + this.cognome.compareTo(p.getCognome()) == 0) {
+               return 0;
+            } else {
+               return this.nome.compareTo(p.getNome()) + this.cognome.compareTo(p.getCognome());
+            }
+         case perEta:
+            if (this.eta == p.getEta()) {
+               return 0;
+            } else {
+               return this.eta - p.getEta();
+            }
+         case perGenere:
+            if (this.sesso == p.getSesso()) {
+               return 0;
+            } else {
+               return this.sesso - p.getSesso();
+            }
+         default:
+            return -50198;
+      }
+   }
+
+   public String toString() {
+      return getNome() + " " + getCognome() + " " + getSesso() + " " + getEta();
+   }
+}
