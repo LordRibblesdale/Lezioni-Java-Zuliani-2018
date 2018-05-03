35439d79fcc5ac5c4e670ce34eb657c25cac404c
Domenico Verde <domenico.verde@aol.com>
2018-05-04 00:15:17 +0200

    - [NOT STARTED] Add Ese39

---
+++ Ese39/MultiMenuTest.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MultiMenuTest extends JFrame {
+   private JMenuBar m;
+   private JToolBar tb;
+   private JTextArea ta;
+   private JMenu jm, color, selection;
+   private JSeparator s;
+
+}
